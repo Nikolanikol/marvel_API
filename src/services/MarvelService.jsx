668a4db654d57c8e9f14a4c78521d9268e3dce7e
Nikolai <krezi3927@gmail.com>
2023-10-07 17:18:20 +0900

    first

---
+++ src/services/MarvelService.jsx
@@ -0,0 +1,50 @@
+
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=a2229ccb06d8d97b20b5dea7ecc03dd7';
+
+    getResource = async (url)=>{
+        let res = await fetch(url)
+
+        if(!res.ok){
+            throw new Error (`Couldnt fetch ${url}, status ${res.status}`)
+        }
+        return await res.json()
+    }
+
+    getAllCharacters = async()=>{
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+        return res.data.results.map(this._transformCharacter)
+    }
+
+    getCharacter = async (id)=>{
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+        return this._transformCharacter(res.data.results[0])
+    }
+    _transformCharacter = (res) =>{
+        let desc = res.description
+        if (res.description.length > 100){
+            desc = res.description.slice(0, 100) + '...'
+        }else{
+            desc = res.description
+        }
+        return {
+            name : res.name,
+            description : desc ? desc : 'not found',
+            thumbnail : res.thumbnail.path + '.' +res.thumbnail.extension,
+            homepage : res.urls[0].url,
+            wiki : res.urls[1].url,
+        }
+    }
+}
+
+// `${this._apiBase} + characters/+${id}+? + ${this._apiKey}`
+// https://gateway.marvel.com:443/v1/public/characters/1011005?apikey=a2229ccb06d8d97b20b5dea7ecc03dd7
+// `${this._apiBase}characters/${id}?&${this._apiBase}`
+
+export default MarvelService
+
+
+
